ba63a0be09bad6796a73ca24dc872c8fc7152877
Yohan <yohanlahiru86@gmail.com>
2025-10-18 19:31:05 +0530

    style added for mobile menu toggle

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -11,10 +11,10 @@ const NavBar = ({menuIcon}:navBarProps)=> {
       {menuIcon && (
         <div className="toggle-menu"  onClick={()=> setActiveMenu(!activeMenu)}>
           <div className="mr-2">Menu</div>
-          <div className="more-menu-icon flex flex-col gap-1 mt-[2px]">
-            <span className="menu-icon w-5 h-[2px] bg-[rgba(10,124,62,0.5)]"></span>
-            <span className="menu-icon w-5 h-[2px] bg-[rgba(10,124,62,0.5)]"></span>
-            <span className="menu-icon w-5 h-[2px] bg-[rgba(10,124,62,0.5)]"></span>
+          <div className="more-menu-icon relative">
+            <span className="menu-icon first"></span>
+            <span className="menu-icon middle"></span>
+            <span className="menu-icon last"></span>
           </div>
         </div>
         
